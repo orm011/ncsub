1e9e27332f2cc79ad0c955c2d7f74dd13fab29e4
Oscar Moll <rimoll@gmail.com>
2026-07-30 14:54:49 -0700

    Add: binary-tree-inorder-traversal - submission-1

---
+++ Data Structures & Algorithms/binary-tree-inorder-traversal/submission-1.py
@@ -0,0 +1,21 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def inorderTraversal(self, root: Optional[TreeNode]) -> List[int]:
+        # in order traversal. 
+        # how is it different from pre-order traversal 
+        # or post order traversal.
+        # left node << node << right node.
+        # does this extend to arbitrary graph? eg ternary tree. what's the order?
+
+        if not root:
+            return []
+
+        left = self.inorderTraversal(root.left)
+        right = self.inorderTraversal(root.right)
+        return left + [root.val] + right
+        #return [*left, root.val, *right]
