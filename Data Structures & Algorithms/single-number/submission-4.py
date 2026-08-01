631881c66d1f23256effbabaaa59728df9552728
Oscar Moll <rimoll@gmail.com>
2026-08-01 08:30:49 -0700

    Add: single-number - submission-4

---
+++ Data Structures & Algorithms/single-number/submission-4.py
@@ -0,0 +1,9 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # now with O(1) space complexity.
+        # cannot build a separate set.
+        total = 0
+        for n in nums:
+            total ^= n
+
+        return total
