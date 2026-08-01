74a320a3fbd7cc29a86cc7a4b7cc014154b97c26
Oscar Moll <rimoll@gmail.com>
2026-08-01 08:24:08 -0700

    Add: single-number - submission-1

---
+++ Data Structures & Algorithms/single-number/submission-1.py
@@ -0,0 +1,14 @@
+class Solution:
+    def singleNumber(self, nums: List[int]) -> int:
+        # every number appears twice, except one.
+        # to do this in one pass, lets build a hash as we go.
+        seen = set({})
+        for n in nums:
+            if n not in seen: # first time, track
+                seen.add(n)
+            else: # second time, remove as possibility
+                seen.remove(n)
+
+        return next(iter(seen))
+
+        
