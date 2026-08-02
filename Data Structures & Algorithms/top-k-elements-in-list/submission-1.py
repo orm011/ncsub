8e1de1a78a436c12cfb58b55292e6f073deaebc4
Oscar Moll <rimoll@gmail.com>
2026-08-02 13:24:54 -0700

    Add: top-k-elements-in-list - submission-1

---
+++ Data Structures & Algorithms/top-k-elements-in-list/submission-1.py
@@ -0,0 +1,34 @@
+import heapq
+class Solution:
+    def topKFrequent(self, nums: List[int], k: int) -> List[int]:
+        # option 1: sort nums, iterate until have k distinct. 
+        # Actualy wrong. its notk highest, its k highest *count*
+        #
+        # option 2: hash nums, keep count, then locate the highest k count. naive is iteration over collection, and sorting the counts.
+        # option 3: slight refinement on option 2, use a heap on the counts, to get top k highest only.  # ie, heapify the counts.  This has k log (num differnt counts). 
+
+        # this one went smoothly. was at risk of solving the wrong
+        # problem. the naive approach can help make sure one understands the problem well.
+        # runtime: O(n) building the counts. 
+        # O(n) heapifying the counts.
+        # O(log num counts) querying k times.
+        # the most number of unique counts that fits in n elemments
+        # would be O $sqrt(n)$
+
+        counts = {}
+        for n in nums:
+            if n not in counts:
+                counts[n] = 1
+            else:
+                counts[n] += 1
+
+        counted = [(-count, num) for (num,count) in counts.items()]
+        heapq.heapify(counted)
+        ans = []
+
+        while counted and len(ans) < k:
+            (key, val) = heapq.heappop(counted)
+            ans.append(val)
+
+        return ans
+        
