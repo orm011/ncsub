14b3ffd35d0575d83cc7a5ba85d99386f398522d
Oscar Moll <rimoll@gmail.com>
2026-08-05 07:09:06 -0700

    Add: car-pooling - submission-2

---
+++ Data Structures & Algorithms/car-pooling/submission-2.py
@@ -0,0 +1,41 @@
+import heapq
+
+class Solution:
+    def carPooling(self, trips: List[List[int]], capacity: int) -> bool:
+        # only doable if
+        # capacity >= carry[i] for all i. where carry is the amount that must
+        # be carried. carry[i] is the sum of numPassengers[j] where j<=i minus
+        # the sum of numPassengers[k] where k <= i and to[k] <= i
+        # lets sort trips by from[] field. 
+        trips.sort(key=lambda trip: trip[1])
+        current = 0 # current number of passengers in car.
+        # idea: go along the trips list in "from" order.
+        # drop all passengers that needed to be dropped before or at this station.
+        # add passengers as specified on each step.
+        # if current crosses capacity, it cannot be done.
+
+        pending_tos = [] # we will use a heap since we need to only drop off the 
+        # earliest toi destinations
+        for i in range(len(trips)):
+            numPi, fromi, toi = trips[i]
+
+            while pending_tos and pending_tos[0][0] <= fromi:
+                (drop_toi, count) = heapq.heappop(pending_tos)
+                current -= count
+
+            if current + numPi > capacity:
+                return False
+
+            # add to drop off schedule
+            heapq.heappush(pending_tos, (toi, numPi))
+            current += numPi
+
+        
+        return True
+        
+        # analysis: O(nlog n) time from sorting the trips.
+        # O(n) iterations in loop.
+        # plus O(log n) from heap at most O(n) heap pops.
+        # total time; O(nlog n )
+        # total space: O(n) (heap)
+        
