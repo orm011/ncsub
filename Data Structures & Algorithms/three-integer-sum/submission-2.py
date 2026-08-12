7c11332e9f23daf24521437221a7660851048791
Oscar Moll <rimoll@gmail.com>
2026-08-12 10:34:45 -0700

    Add: three-integer-sum - submission-2

---
+++ Data Structures & Algorithms/three-integer-sum/submission-2.py
@@ -0,0 +1,55 @@
+from collections import Counter
+
+class Solution:
+    # one approach:
+    # triple nested loop checking sums of non equal indices.
+    # a set of existing results to dedup
+    # runtime O(n^3)
+    # space for results could be also O(n^3) in general.
+    # alternative: two levels of recursion. and match 
+    # the third number exactly: O(n^2)
+
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # cannot re-use an index AND multiple index combinations
+        # could result in a duplicate if the values are repeated
+        nums.sort() # in place sorting.
+        numset = set(nums)
+        solutions = []
+        current = []
+        def build_solns(*, r: int, target: int, index: int) -> None: 
+            # r remaining levels. 
+            # index: allowed lowest index
+            nonlocal current
+
+            if r == 1 and index < len(nums) and target >= nums[index] and target in numset:
+                current.append(target)
+                solutions.append(current.copy())
+                current.pop()
+            elif r >= 2:
+                i = index
+                while i < len(nums): 
+                    # only explore indices beyond current
+                    k = nums[i]
+                    current.append(k)
+                    build_solns(r=r - 1, target=target - k, index=i + 1)
+                    # can repeat value within same solution
+                    # but need to skip forward to next unique value after using it.
+                    current.pop()
+                    while i < len(nums) and nums[i] == k:
+                        i+=1 # skip forward to next possible value
+
+        build_solns(r=3, target=0, index=0)
+        # ensured no duplicate indices but not globally unique solutions.
+        # now, we have gone the other way. too aggressive.
+        # we do allow repeated values, as long as there are multiple copies.
+        # now we do allow repeated values, but are running into dups again.
+        # easy solution: just remove dupes using a solution set.
+
+        # total complexity: O(nlogn) + O(n) + O(n^2) (we really remove half the space though). 
+        # total space: O(n^2) for the solution set.
+
+        return solutions
+
+
+
+        
