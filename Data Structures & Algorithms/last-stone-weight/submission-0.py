bce237ca0e40c033f7a07b7987ca8c3a5d1a2cf3
Oscar Moll <rimoll@gmail.com>
2026-08-17 19:02:57 -0700

    Add: last-stone-weight - submission-0

---
+++ Data Structures & Algorithms/last-stone-weight/submission-0.py
@@ -0,0 +1,26 @@
+import heapq
+
+class Solution:
+    def lastStoneWeight(self, stones: List[int]) -> int:
+        # we replace 2 heaviest y >= x with 
+        # one y - x one (unless 0) eventually we get down 
+        # to either a lone small number left (if the other two are gone before)
+        # or otherwise, 0 iff last two are identical.
+        # invariants:
+        # reduce number of stones by 1 or 2 per round.
+        # we cannot know which will be the next round...
+        # at every step get the two top stones, check what will replace them.
+        # can be done with a heap
+        heapq.heapify_max(stones)
+        while stones:
+            if len(stones) == 1:
+                return stones[0]
+            top1 = heapq.heappop_max(stones)
+            top2 = heapq.heappop_max(stones)
+            diff = top1 - top2 
+            if diff > 0:
+                heapq.heappush_max(stones, diff)
+
+
+        return 0
+        
